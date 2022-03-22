1a23893027eed68a6998585b8d14f00abd56c674
Han Y.B <yanbohan98@gmail.com>
2022-03-22 16:23:48 +0800

    refactor(oss): ossservice.

---
+++ src/main/java/top/horizonask/hoawiki/ossstorage/service/OssService.java
@@ -0,0 +1,13 @@
+package top.horizonask.hoawiki.ossstorage.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @description:
+ * @time: 2022/2/11 3:55
+ */
+
+@Service
+public interface OssService {
+
+}
